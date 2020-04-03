b8cb4b5f5e08209e8941eed5811861b8b00a98fa
Ismel <ismelfigueroa7@gmail.com>
2020-04-02 21:43:32 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import logo from "./logo.svg";
+import Form from './Form';
+import './style.css';
+import Axios from "axios";
+
+
+export default class App extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.getCountryData = this.getCountryData.bind(this);
+    }
+  state = {
+    confirmed: 0,
+    recovered: 0,
+    deaths: 0,
+    countries: []
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    const resApi = await Axios.get("https://covid19.mathdro.id/api");
+    const resCountries = await Axios.get("https://covid19.mathdro.id/api/countries");
+    const countries = resCountries.data.countries
+    console.log('countries', resCountries.data.countries)
+    this.setState({
+        confirmed: resApi.data.confirmed.value,
+        recovered: resApi.data.recovered.value,
+        deaths: resApi.data.deaths.value,
+        countries: countries,
+    });
+  }
+
+async getCountryData(e) {
+  if (e.target.value === "Worldwide") {
+    return this.getData();
+  }
+  try {
+   const res = await Axios.get(`https://covid19.mathdro.id/api/countries/${e.target.value}`);
+    this.setState({
+        confirmed: res.data.confirmed.value,
+        recovered: res.data.recovered.value,
+        deaths: res.data.deaths.value,
+});
+  }
+  catch (err) {
+    if (err.response.status === 404)
+    this.setState({
+      confirmed: "No data available",
+      recovered: "No data available",
+      deaths: "No data available",
+  });
+}
+  }
+
+renderCountryOptions() {
+      return this.state.countries.map((country, i) => {
+        return <option key={i}>{country.name}</option>
+      });
+}
+
+  render() {
+  
+    return (
+      
+    <div className="box container">
+      <h1>COVID-19 Update</h1>
+      
+<select className="dropdown" onChange={this.getCountryData}>
+  <option>Worldwide</option>
+  {this.renderCountryOptions()}
+</select>
+    <div className="flex">
+      <div className="box confirmed">
+        <h3>Confirmed cases</h3>
+      <h4>{this.state.confirmed}</h4>
+      </div>
+      <div className="box recovered">
+      <h3>Recovered cases</h3>
+      <h4>{this.state.recovered}</h4>
+      </div>
+      <div className="box deaths">
+      <h3>Deaths cases</h3>
+      <h4>{this.state.deaths}</h4>
+      </div>
+      </div>   
+      <Form 
+      firstName="Ismel" 
+      lastname="Figueroa"
+      userName="ismelfigueroa"
+      email="ismelfigueroa7@gamil.com"
+      password="bootcamp"/>
+    </div>
+    );
+  }
+}
+
+
